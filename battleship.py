793a82f5ab635ff91835fe04d0b26dde7f2aa565
gokdenizyilmaz <60828507+gokdenizyilmaz@users.noreply.github.com>
2023-06-29 19:35:04 +0300

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -45,6 +45,7 @@ coordinate24 = input("Enter coordinate 4 for player 2: ")
 
 player2 = Battleship(coordinate21, coordinate22, coordinate23, coordinate24)
 
+print("\n")
 
 coordinates = ["a1","a2","a3","a4","a5","a6","a7","b1","b2","b3","b4","b5","b6","b7","c1","c2","c3","c4","c5","c6","c7",
 "d1","d2","d3","d4","d5","d6","d7","e1","e2","e3","e4","e5","e6","e7","f1","f2","f3","f4","f5","f6","f7",
